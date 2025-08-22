3d6a8fcc33dcbd763fd63f4852df68f850e95c6a
WirkLichKeit1 <luizfagnersilva87@gmail.com>
2025-08-22 18:51:27 -0300

    Update banco de dados.sql

--- banco de dados.sql
+++ banco de dados.sql
@@ -1,20 +1,22 @@
-create database bot;
+create database if not exists bot;
 
 use bot;
 
-create table usuarios (
+create table if not exists usuarios (
 	id int auto_increment primary key,
     nome varchar(100) not null,
-    email varchar(100) not null unique,
-    senha varchar(255) not null,
+    email varchar(100) unique,
+    senha varchar(220) not null,
     criado_em timestamp default current_timestamp
 );
 
-create table logs (
+create table if not exists logs (
 	id int auto_increment primary key,
     usuario_id int,
     pergunta text not null,
     resposta text not null,
     criado_em timestamp default current_timestamp,
-	foreign key(usuario_id) references usuarios(id) on delete cascade
+    foreign key(usuario_id) references usuarios(id) on delete cascade
 );
+
+create index idx_logs_usuarios_id on logs(usuario_id);
