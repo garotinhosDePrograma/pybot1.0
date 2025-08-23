5efb455dc732b5d414e971d921efbfeaf0ca36a4
diegovsk00 <diegolegal62@gmail.com>
2025-08-22 21:08:30 -0300

    Update user_repository.py

--- projetoBack/repository/user_repository.py
+++ projetoBack/repository/user_repository.py
@@ -1,4 +1,3 @@
-from flask import Blueprint, request, jsonify
 from utils.db import get_connection
 
 def find_by_email(email: str):
@@ -8,6 +7,15 @@ def find_by_email(email: str):
     usuario = cursor.fetchone()
     cursor.close()
     conn.close()
+    return usuario
+
+def find_by_id(usuario_id: int):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("select * from usuarios where id = %s", (usuario_id))
+    usuario = cursor.fetchone()
+    cursor.close()
+    conn.close
     return usuario
 
 def create_usuario(nome: str, email: str, senha_hash: str):
@@ -22,3 +30,15 @@ def create_usuario(nome: str, email: str, senha_hash: str):
     cursor.close()
     conn.close()
     return usuario_id
+
+def update_senha(usuario_id: int, nova_senha_hash: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "update usuarios set senha = %s where id = %s",
+        (nova_senha_hash, usuario_id)
+    )
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return True
